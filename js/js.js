36e974caa10af69b5e2bf272e9193d8157a5eca0
Utsho11 <utshoroy521@gmail.com>
2023-02-18 21:49:27 +0600

    final commit

--- js/js.js
+++ js/js.js
@@ -1,78 +1,133 @@
 document.getElementById('blog-btn').addEventListener('click',function(){
-    window.location.href = 'QnA.html';
+    window.location.href = "QnA.html"
 })
 
 function getInputValueById(inputId){
     const inputField = document.getElementById(inputId);
     const inputFieldString = inputField.value;
     const inputValue = parseFloat(inputFieldString);
+    inputField.value = "";
     return inputValue;
 }
-
-function displayData(itemName,Area){
+let serial = 0;
+function displayData(itemName,area){
     const container = document.getElementById('table-container')
 
     const tr = document.createElement("tr");
     tr.innerHTML = `
-    <td>${1}.</td>
+    <td>${serial}.</td>
     <td>${itemName}</td>
-    <td>${Area} cm<sup>2</sup></td>
+    <td>${area} cm<sup>2</sup></td>
     <td><button class="btn btn-primary">convert m<sup>2</sup></button></p></td>
     `;
     container.appendChild(tr);
 }
 
-
 document.getElementById('triangle-btn').addEventListener('click',function(){
     const inputOne = getInputValueById('triangle-base');
     const inputTwo = getInputValueById('triangle-height');
+    serial += 1;
 
-    if(inputOne === Number & inputTwo === Number){
+    if(isNaN(inputOne) || isNaN(inputTwo)){
+        alert('plz enter valid integer');
+    }
+    else{
+    const areaTotal = 0.5 * inputOne * inputTwo;
+    const area = areaTotal.toFixed(2);
+    
     const itemName = document.getElementById('first-item-name').innerText;
 
-    const AreaValue = inputOne * inputTwo * 0.5;
-    const Area = AreaValue.toFixed(2);
-
-    displayData(itemName,Area);
+    displayData(itemName,area);
     }
-    else
-    alert('plz enter valid integers');
-    
+
 })
 document.getElementById('rectangle-btn').addEventListener('click',function(){
     const inputOne = getInputValueById('rectangle-length');
     const inputTwo = getInputValueById('rectangle-width');
+    serial += 1;
 
-    if(inputOne === Number & inputTwo === Number){
+    if(isNaN(inputOne) || isNaN(inputTwo)){
+        alert('plz enter valid integer');
+    }
+    else{
+    const areaTotal = inputOne * inputTwo;
+    const area = areaTotal.toFixed(2);
     
     const itemName = document.getElementById('second-item-name').innerText;
 
-    const AreaValue = inputOne * inputTwo;
-    const Area = AreaValue.toFixed(2);
+    displayData(itemName,area);
+    }
 
-    displayData(itemName,Area);
-}
-else
-alert('plz enter valid integers');
-    
 })
 document.getElementById('parallelogram-btn').addEventListener('click',function(){
-    const inputOne = getInputValueById('parallelogram-base');
-    const inputTwo = getInputValueById('parallelogram-height');
+    const inputOne = getInputValueById('parallelogram-height');
+    const inputTwo = getInputValueById('parallelogram-base');
+    serial += 1;
 
-
-   
-if(inputOne === Number & inputTwo === Number)
-{
-const itemName = document.getElementById('third-item-name').innerText;
-const AreaValue = inputOne * inputTwo;
-const Area = AreaValue.toFixed(2);
-
-displayData(itemName,Area);}
-        
+    if(isNaN(inputOne) || isNaN(inputTwo)){
+        alert('plz enter valid integer');
+    }
+    else{
+    const areaTotal = inputOne * inputTwo;
+    const area = areaTotal.toFixed(2);
     
-    
-    else
-    alert('plz enter valid integers');
-    })
+    const itemName = document.getElementById('third-item-name').innerText;
 
+    displayData(itemName,area);
+    }
+
+})
+document.getElementById('rhombus-btn').addEventListener('click',function(){
+    const inputOne = getInputValueById('rhombus-d1');
+    const inputTwo = getInputValueById('rhombus-d2');
+    serial += 1;
+
+    if(isNaN(inputOne) || isNaN(inputTwo)){
+        alert('plz enter valid integer');
+    }
+    else{
+    const areaTotal = 0.5 * inputOne * inputTwo;
+    const area = areaTotal.toFixed(2);
+    
+    const itemName = document.getElementById('forth-item-name').innerText;
+
+    displayData(itemName,area);
+    }
+
+})
+document.getElementById('pentagon-btn').addEventListener('click',function(){
+    const inputOne = getInputValueById('pentagon-p');
+    const inputTwo = getInputValueById('pentagon-b');
+    serial += 1;
+
+    if(isNaN(inputOne) || isNaN(inputTwo)){
+        alert('plz enter valid integer');
+    }
+    else{
+    const areaTotal = 0.5 * inputOne * inputTwo;
+    const area = areaTotal.toFixed(2);
+    
+    const itemName = document.getElementById('fifth-item-name').innerText;
+
+    displayData(itemName,area);
+    }
+
+})
+document.getElementById('ellipse-btn').addEventListener('click',function(){
+    const inputOne = getInputValueById('ellipse-a');
+    const inputTwo = getInputValueById('ellipse-b');
+    serial += 1;
+
+    if(isNaN(inputOne) || isNaN(inputTwo)){
+        alert('plz enter valid integer');
+    }
+    else{
+    const areaTotal = 3.1416 * inputOne * inputTwo;
+    const area = areaTotal.toFixed(2);
+    
+    const itemName = document.getElementById('sixth-item-name').innerText;
+
+    displayData(itemName,area);
+    }
+
+})
